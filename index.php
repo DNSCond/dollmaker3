c2d1e4342c085f057fdcb3944d177870bd650017
DNSCond <condi7002@gmail.com>
2026-06-17 11:07:29 +0200

    sonethimh

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ require_once "preprocessor.php";
 global $colors;
 $links = create_head2('Character Customizer Version5', [
         'base' => '/dollmaker3/', 'desc' => 'ANTRequest.nl\'s Character Creator, create your ANTRequest.nl\'s Character lookalike here'], [
-        new ANTNavLinkTag('stylesheet', ['styles.css', 'ddDL-table.css',]),
+        new ANTNavLinkTag('stylesheet', ['styles.css', 'ddDL-table.css', 'display.css']),
         new ANTNavIStyle('div.divs.nav-home{max-width:unset;}'), new ANTNavIStyle('h1{margin-top:0;}'),
         new ANTNavLinkTag('canonical', "https://localhost/dollmaker3/{$GLOBALS['canonicalFullString']}"),
     //new ANTNavLinkTag('canonical', "https://antrequest.nl/dollmaker3/{$GLOBALS['canonicalFullString']}"),
@@ -30,7 +30,6 @@ $links = create_head2('Character Customizer Version5', [
                 "<meta property=og:image content=\"https://localhost/dollmaker3/endpoint.svg.php?" . htmlspecialchars12($_SERVER['QUERY_STRING']) . '">' .
                 "<meta property=og:image:width content=800><meta property=og:image:height content=1280>" .
                 "<meta property=og:image:type content=image/svg+xml>"),
-        new ANTNavArbitraryHTML('preload', '<link rel=preload href=ddDL-table.css as=style><link rel=preload href=display.css as=style>'),
 ], [ANTNavHome(),
         new ANTNavOption('/dollmaker3/', '/dollmaker2/icon/endpoint.php?preset=Bee',
                 'dollmakerV4 ANT', new Color('a68300'),
@@ -66,31 +65,11 @@ $links = create_head2('Character Customizer Version5', [
             })() ?></div>
     </form>
 </div>
-<template id=CharacterDisplay_template>
-    <!--<?= 'PHPTP';
-    ob_start(); ?>-->
-    <link rel=stylesheet href=ddDL-table.css>
-    <link rel=stylesheet href=display.css>
-    <div style=padding:0.5em>
-        <span><slot name=accessory-name>Accessory Name</slot></span>
-    </div>
-    <div><?= /** @noinspection RequiredAttributes */
-        "<img data-img-src alt=\"Equip \$AccessoryName\" width=800 height=1280 class=store-img>" ?></div>
-    <dl class=descLi style=border-bottom:none;border-left:none;border-right:none>
-        <div data-key=cost>
-            <dt>cost</dt>
-            <dd>
-                <slot name=cost>Unknown</slot>
-        </div>
-    </dl>
-    <!--<?= 'PHPTP';
-    $templateContent = ob_get_flush();
-    $templateContent = preg_replace('/\\s+/', ' ', $templateContent);
-    $templateContent = preg_replace('/\\s+<!--PHPTP$/D', '', $templateContent);
-    $templateContent = preg_replace('/^-->\\s+/', '', $templateContent) ?>-->
-</template>
-<div class=divs><?= "\n";
+<form class=divs method=post action=oninput.php><?= "\n";
     $afterColors = (7 * 4);
+    foreach ($colors as $name => $color) {
+        echo "<input type=hidden value=$color name=$name>\n";
+    }
     $filegc = file_get_contents(__DIR__ . '/store/assets.json');
     if ($filegc) $json = json_decode($filegc, true); else $json = array('failure');
     echo '<script type=application/json is=output-script>' . json_encode($json, JSON_INVALID_UTF8_SUBSTITUTE) . '</script>';
@@ -120,53 +99,18 @@ $links = create_head2('Character Customizer Version5', [
 
         // 4. Write the Asset Option
         $view->setUint8($afterColors + 7, 0);
-
-        //$htmlName.=' '.+$matches[1];
-        $img_src = htmlspecialchars12("endpoint.svg.php?dna=v1u.{$view->toBase64URL()}");
-        $content = str_replace('data-img-src', "src=\"$img_src\"", $templateContent);
-        $content = str_replace('$AccessoryName', $htmlName, $content);
         if (array_key_exists('cost', $item)) $cost = match ($item['cost']) {
             true => 'default',
             false => 'Off Sale',
             0 => 'Free',
             default => "{$item['cost']} units",
         }; else $cost = 'Free';
-        echo "<character-display img-src=\"$img_src\"><template shadowrootmode=open>$content</template>" .
-                "<span slot=accessory-name>$htmlName</span><span slot=cost>$cost</span></character-display>\n";
-    } ?></div>
-<script type=module>
-    class CharacterDisplay extends HTMLElement {
-        static get observedAttributes() {
-            return Array();//['img-src'];
-        }
-
-        get [Symbol.toStringTag]() {
-            return 'HTML_' + this.constructor.name?.toUpperCase();
-        }
-
-        constructor() {
-            super();
-            //const clone = document.getElementById('CharacterDisplay_template').content.cloneNode(true);
-            //this.attachShadow({mode: 'open'}).append(clone);
-        }
-
-        connectedCallback() {
-            this.shadowRoot.querySelector('img.store-img').src = this.getAttribute('img-src');
-            /*const attr = this.getAttribute('asset-data');
-            if (attr) {
-                const pre = document.createElement('pre'),
-                    code = document.createElement('code');
-                pre.append(code);
-                code.append(JSON.stringify(JSON.parse(attr), null, 2));
-                this.shadowRoot.append(pre);
-            }*/
-        }
-
-        attributeChangedCallback(name, _oldValue, newValue) {
-            this.shadowRoot.querySelector('img.store-img').src = newValue;
-        }
+        //$htmlName.=' '.+$matches[1];
+        $img_src = htmlspecialchars12("endpoint.svg.php?dna=v1u.{$view->toBase64URL()}");
+        $inputname = "asset-$matches[1]";
+        echo "<character-display><div style=padding:0.5em><label for=$inputname>$htmlName <input type=checkbox" .
+                " value=$matches[1] id=$inputname name=assets[]></label></div><div><img src=\"$img_src\" alt=\"" .
+                "Equip $htmlName\" width=800 height=1280 class=store-img></div><dl class=descLi><div data-key="
+                . "cost><dt>cost<dd><slot name=cost>$cost</slot></div></dl></character-display>";
     }
-
-    // customElements.define('character-display', CharacterDisplay);
-    // await customElements.whenDefined('character-display');
-</script>
+    echo "\n<div style='margin: 1em 0 1em 1em'><button type=submit>apply preview</button></div>"; ?></form>
--- oninput.php
+++ oninput.php
@@ -9,7 +9,9 @@ $keys = array_keys($colors);
 if (preg_match('/^' . (str_repeat('#?([a-f-A-F0-9]{6});', 7)) . '$/D',
     implode(';', array_map(fn($color) => is_string($_POST[$color]) ? $_POST[$color] : 'Invalid-',
         $keys)) . ';', $matches)) {
-    $result = new BinaryView((count($keys) * 4) + 1 + 1);
+    //$result = new BinaryView((count($keys) * 4) + 1 + 1);
+    // Sets the buffer size to exactly 29 bytes (28 bytes for colors + 1 byte for flags)
+    $result = new BinaryView((count($keys) * 4) + 1);
     foreach ($keys as $index => $_colorKey) {
         RGBToRGBA32($result, (int)hexdec($matches[$index + 1]), $index * 4);
     }
